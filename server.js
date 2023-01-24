9110930b3e5d4a48d96f781411868d2ffd19dc65
Anupam Debnath <65649115+anupam6335@users.noreply.github.com>
2023-01-24 12:06:33 +0530

    dev script add

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const dbConfig = require("./config/dbConfig");
+const path = require('path');
 app.use(express.json());
 const userRoute = require("./routes/userRoute");
 const adminRoute = require("./routes/adminRoute");
@@ -11,6 +12,12 @@ app.use("/api/user", userRoute);
 app.use("/api/admin", adminRoute);
 app.use("/api/doctor", doctorRoute);
 
+
+// static files accesss
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 console.log(process.env.MONGO_URL);
 const port = process.env.PORT || 5000;
